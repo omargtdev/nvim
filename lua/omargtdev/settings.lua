658bfb415b13257c0399e17a9e476939947c5fd3
omargtdev <omargtdev@gmail.com>
2023-12-23 18:36:45 -0500

    Migrating to lazy-git

--- lua/omargtdev/settings.lua
+++ lua/omargtdev/settings.lua
@@ -1,18 +1,23 @@
 vim.opt.nu = true
 vim.opt.rnu = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+-- using vi-sleuth instead
+--vim.opt.tabstop = 4
+--vim.opt.softtabstop = 4
+--vim.opt.shiftwidth = 4
+--vim.opt.expandtab = true
+--vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 
-local isWindowsOS = string.find(vim.loop.os_uname().sysname:lower(), "windows")
+local isWindowsOS = string.find(
+    vim.loop.os_uname().sysname:lower(),
+    "windows"
+)
+
 local home = isWindowsOS and "USERPROFILE" or "HOME"
 vim.opt.undodir = os.getenv(home) .. "/.vim/undodir"
 vim.opt.undofile = true
@@ -29,3 +34,13 @@ vim.opt.updatetime = 50
 
 -- Util for following good wrapping practices
 vim.opt.colorcolumn = "80"
+
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
