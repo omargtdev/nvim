7a2e430756751cdd2f6629045d5dc8caeaeeea42
omargtdev <omargtdev@gmail.com>
2023-12-23 18:35:35 -0500

    Deleting after dir config

--- after/plugin/fugitive.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<leader>gs", function() 
-	if not pcall(vim.cmd.Git) then
-		print("Does not exist a git directory in this project or fugitive is do not installed.")
-	end
-end)
--- after/plugin/undotree.lua
+++
@@ -1,3 +0,0 @@
-if vim.fn.exists(":UndotreeToggle") == 0 then return end
-
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
