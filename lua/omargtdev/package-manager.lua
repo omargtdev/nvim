5af99c4e21f506d4454b0a01e22efdf6705a17dd
omargtdev <omargtdev@gmail.com>
2024-02-05 00:11:10 -0500

    lsp: add diagnostic keymap

--- lua/omargtdev/package-manager.lua
+++ lua/omargtdev/package-manager.lua
@@ -67,6 +67,7 @@ local on_attach = function(client, bufnr)
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
+  nmap("gl", vim.diagnostic.open_float, "Open Diagnostic Float")
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
